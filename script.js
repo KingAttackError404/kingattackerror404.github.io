3ed7f06b3e52d3d4af8cd9f84137d7a68a8cac82
KingAttackError404 <135358752+KingAttackError404@users.noreply.github.com>
2023-09-10 21:33:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,29 @@
-// Ketika dokumen HTML telah dimuat sepenuhnya
-document.addEventListener("DOMContentLoaded", function () {
-document.documentElement.requestFullscreen();
-    // Fungsi untuk menambahkan efek "smooth scrolling" pada tautan internal
-    function smoothScroll(targetId) {
-        const targetElement = document.getElementById(targetId);
-        if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: "smooth",
-            });
-        }
-    }
+// Ambil semua tautan yang merujuk ke halaman internal
+const internalLinks = document.querySelectorAll('a[href^="#"]');
 
-    // Menggunakan event listener untuk menangani klik pada tautan navigasi
-    const navLinks = document.querySelectorAll("nav ul li a");
-    navLinks.forEach((link) => {
-        link.addEventListener("click", function (event) {
-            event.preventDefault(); // Mencegah perilaku default tautan
-            const targetId = link.getAttribute("href").substring(1); // Mengambil ID target dari atribut href
-            smoothScroll(targetId); // Memanggil fungsi smoothScroll
-        });
-    });
-
-    // Fungsi untuk menyalin tautan undangan ke clipboard
-    function copyToClipboard() {
-        const inviteLink = document.getElementById("inviteLink");
-        const textArea = document.createElement("textarea");
-        textArea.value = inviteLink.href;
-        document.body.appendChild(textArea);
-        textArea.select();
-        document.execCommand("copy");
-        document.body.removeChild(textArea);
-        alert("Link copied to clipboard!");
-    }
-
-    // Menggunakan event listener untuk menangani klik pada tombol "Copy Link"
-    const copyButton = document.getElementById("copyButton");
-    if (copyButton) {
-        copyButton.addEventListener("click", function () {
-            copyToClipboard(); // Memanggil fungsi copyToClipboard
-        });
-    }
+// Tambahkan event listener ke semua tautan tersebut
+internalLinks.forEach(link => {
+    link.addEventListener('click', smoothScroll);
 });
+
+// Fungsi untuk mengatur smooth scrolling
+function smoothScroll(e) {
+    e.preventDefault();
+    
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+        const offset = targetElement.getBoundingClientRect().top;
+        const scrollOptions = {
+            behavior: 'smooth',
+            block: 'start'
+        };
+        
+        window.scrollTo({
+            top: offset + window.scrollY,
+            ...scrollOptions
+        });
+    }
+}
+    
