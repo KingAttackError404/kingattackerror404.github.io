ed5ea49ee699b3b4e7fad6bba9812b4bd471d69f
KingAttackError404 <135358752+KingAttackError404@users.noreply.github.com>
2023-09-10 10:03:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// Ketika dokumen HTML telah dimuat sepenuhnya
+document.addEventListener("DOMContentLoaded", function () {
+    // Fungsi untuk menambahkan efek "smooth scrolling" pada tautan internal
+    function smoothScroll(targetId) {
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: "smooth",
+            });
+        }
+    }
+
+    // Menggunakan event listener untuk menangani klik pada tautan navigasi
+    const navLinks = document.querySelectorAll("nav ul li a");
+    navLinks.forEach((link) => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault(); // Mencegah perilaku default tautan
+            const targetId = link.getAttribute("href").substring(1); // Mengambil ID target dari atribut href
+            smoothScroll(targetId); // Memanggil fungsi smoothScroll
+        });
+    });
+
+    // Fungsi untuk menyalin tautan undangan ke clipboard
+    function copyToClipboard() {
+        const inviteLink = document.getElementById("inviteLink");
+        const textArea = document.createElement("textarea");
+        textArea.value = inviteLink.href;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand("copy");
+        document.body.removeChild(textArea);
+        alert("Link copied to clipboard!");
+    }
+
+    // Menggunakan event listener untuk menangani klik pada tombol "Copy Link"
+    const copyButton = document.getElementById("copyButton");
+    if (copyButton) {
+        copyButton.addEventListener("click", function () {
+            copyToClipboard(); // Memanggil fungsi copyToClipboard
+        });
+    }
+});
