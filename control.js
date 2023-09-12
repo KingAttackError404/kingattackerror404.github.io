26d582293dd3e7e200d9cc04e38e860d0af5d2e1
KingAttackError404 <135358752+KingAttackError404@users.noreply.github.com>
2023-09-12 20:40:30 +0700

    Update control.js

--- control.js
+++ control.js
@@ -1,30 +1,30 @@
-    // Fungsi untuk mengarahkan ke halaman yang sesuai berdasarkan status situs
+// Fungsi untuk mengarahkan ke halaman yang sesuai berdasarkan status situs
     function redirectToPage() {
-        const siteStatus = localStorage.getItem('siteStatus'); // Baca status dari localStorage
+        const siteStatus = localStorage.getItem('siteStatus');
         if (siteStatus === 'maintenance') {
             window.location.href = 'maintenance.html';
         } else if (siteStatus === 'under_attack_v1') {
-            window.location.href = '502.html'; // Arahkan ke 502.html
+            window.location.href = '502.html';
         } else if (siteStatus === 'under_attack_v2') {
-            window.location.href = 'v2.html'; // Arahkan ke v2.html
+            window.location.href = 'v2.html';
         } else {
-            window.location.href = 'main.html'; // Arahkan ke main.html (normal)
+            window.location.href = 'main.html'; // Default jika status tidak ada atau normal
         }
     }
 
     // Panggil fungsi pengalihan saat halaman dimuat
     redirectToPage();
 
-    // Variabel untuk menyimpan status sebelumnya
-    let currentStatus = localStorage.getItem('siteStatus');
-
-    // Fungsi untuk memeriksa perubahan status dan melakukan refresh saat diperlukan
+    // Fungsi untuk memeriksa perubahan status dari kontrol
     function checkAndRefreshStatus() {
-        const newStatus = localStorage.getItem('siteStatus'); // Ambil status baru
-        if (newStatus !== currentStatus) {
-            location.reload(); // Lakukan refresh jika terjadi perubahan status
-        }
+        const currentStatus = localStorage.getItem('siteStatus');
+        setInterval(function () {
+            const newStatus = localStorage.getItem('siteStatus');
+            if (newStatus !== currentStatus) {
+                location.reload(); // Lakukan refresh hanya jika ada perubahan status
+            }
+        }, 1000); // Periksa setiap 1 detik
     }
 
-    // Panggil fungsi pemantauan perubahan status secara berulang
-    setInterval(checkAndRefreshStatus, 1000); // Periksa setiap 1 detik
+    // Panggil fungsi pemantauan perubahan status
+    checkAndRefreshStatus();
