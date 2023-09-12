47536e88ffcbc8c418b1c0166fbc7a8781c938f1
KingAttackError404 <135358752+KingAttackError404@users.noreply.github.com>
2023-09-12 20:27:46 +0700

    Update control.js

--- control.js
+++ control.js
@@ -1,34 +1,30 @@
-        // Fungsi untuk mengarahkan ke halaman yang sesuai berdasarkan status situs
-        function redirectToPage() {
-            const siteStatus = localStorage.getItem('siteStatus'); // Baca status dari localStorage
-            if (siteStatus === 'maintenance') {
-                window.location.href = 'maintenance.html';
-            } else if (siteStatus === 'under_attack_v1') {
-                window.location.href = '502.html'; // Arahkan ke 502.html
-            } else if (siteStatus === 'under_attack_v2') {
-                window.location.href = 'v2.html'; // Arahkan ke v2.html
-            } else {
-                window.location.href = 'main.html'; // Arahkan ke main.html (normal)
-            }
+    // Fungsi untuk mengarahkan ke halaman yang sesuai berdasarkan status situs
+    function redirectToPage() {
+        const siteStatus = localStorage.getItem('siteStatus'); // Baca status dari localStorage
+        if (siteStatus === 'maintenance') {
+            window.location.href = 'maintenance.html';
+        } else if (siteStatus === 'under_attack_v1') {
+            window.location.href = '502.html'; // Arahkan ke 502.html
+        } else if (siteStatus === 'under_attack_v2') {
+            window.location.href = 'v2.html'; // Arahkan ke v2.html
+        } else {
+            window.location.href = 'main.html'; // Arahkan ke main.html (normal)
         }
+    }
 
-        // Fungsi untuk memeriksa status situs secara berulang
-        function checkSiteStatus() {
-            const currentStatus = localStorage.getItem('siteStatus'); // Baca status dari localStorage
-            const previousStatus = localStorage.getItem('previousSiteStatus'); // Baca status sebelumnya
+    // Panggil fungsi pengalihan saat halaman dimuat
+    redirectToPage();
 
-            if (currentStatus !== previousStatus) {
-                // Jika status berubah, maka perbarui halaman
-                localStorage.setItem('previousSiteStatus', currentStatus); // Simpan status saat ini sebagai status sebelumnya
-                redirectToPage(); // Arahkan ke halaman yang sesuai
-            } else {
-                // Jika status tetap sama, periksa kembali setelah beberapa detik
-                setTimeout(checkSiteStatus, 1000); // Cek setiap 1 detik
+    // Fungsi untuk memeriksa perubahan status dan melakukan refresh saat diperlukan
+    function checkAndRefreshStatus() {
+        const currentStatus = localStorage.getItem('siteStatus'); // Ambil status saat ini
+        setInterval(function () {
+            const newStatus = localStorage.getItem('siteStatus'); // Ambil status baru
+            if (newStatus !== currentStatus) {
+                location.reload(); // Lakukan refresh jika terjadi perubahan status
             }
-        }
+        }, 1000); // Periksa setiap 1 detik
+    }
 
-        // Panggil fungsi pengalihan saat halaman dimuat
-        redirectToPage();
-
-        // Panggil fungsi pemantauan saat halaman dimuat
-        checkSiteStatus();
+    // Panggil fungsi pemantauan perubahan status
+    checkAndRefreshStatus();
